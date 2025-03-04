eb22aedff7fdfaee2eed7e8797792f4b87e05d72
Brian454-nexus <brian.terer@student.moringaschool.com>
2025-03-04 21:39:22 +0300

    Added Arithmetics

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+//Multiply
+let num1 = 2;
+let num2 =  31;
+let multiply = num1 * num2;
 
-// Write your code here
+//Random
+let random = Math.floor(Math.random() * 10) + 1;
+
+//Mod
+let num3 = 9;
+let num4 = 5;
+let mod = num3 % num4;
+
+//Max
+let max = Math.max(1, 2, 3, 4, 20);
